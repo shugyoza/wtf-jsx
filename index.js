94f41df3db98297c350ca8bd2d9baf67921dc0b0
Stephen Hanjaya <stephenhanjaya@gmail.com>
2022-03-28 15:41:36 -0700

    adding .babelrc, "start": "nodemon.." in package.json, npm install --save-dev @babel/cli

---
+++ index.js
@@ -0,0 +1,7 @@
+const babel = require('@babel/core');
+import { transform } from '@babel/core';
+import * as babel from '@babel/core';
+
+require('@babel/core').transform('code', {
+    presets = ['@babel/preset-env'],
+});
